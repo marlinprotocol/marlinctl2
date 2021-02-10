0b027835a0c606ac9a970000e3de58cc5ac1f835
Supragya Raj <supragyaraj@gmail.com>
2021-02-10 21:15:26 +0530

    rundir changed to currentuser homedir

--- modules/runner/gateway_dot/linux-amd64.supervisor.runner01.go
+++ modules/runner/gateway_dot/linux-amd64.supervisor.runner01.go
@@ -121,10 +121,15 @@ func (r *linux_amd64_supervisor_runner01) Create(runtimeArgs map[string]string) 
 		return errors.New("Resource file already exisits, cannot create a new instance: " + GetResourceFileLocation(r.Storage, r.InstanceId))
 	}
 
+	currentUser, err := util.GetUser()
+	if err != nil {
+		return err
+	}
+
 	substitutions := resource{
 		"linux-amd64.supervisor.runner01", r.Version, time.Now().Format(time.RFC822Z),
-		gatewayProgramName + r.InstanceId, defaultUser, "/", r.Storage + "/" + r.Version + "/" + gatewayName, "", "",
-		bridgeProgramName + r.InstanceId, defaultUser, "/", r.Storage + "/" + r.Version + "/" + bridgeName, "", "", "", "", "", "", "",
+		gatewayProgramName + r.InstanceId, defaultUser, currentUser.HomeDir, r.Storage + "/" + r.Version + "/" + gatewayName, "", "",
+		bridgeProgramName + r.InstanceId, defaultUser, currentUser.HomeDir, r.Storage + "/" + r.Version + "/" + bridgeName, "", "", "", "", "", "", "",
 	}
 
 	for k, v := range runtimeArgs {
